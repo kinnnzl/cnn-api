30981dd2412521b64de8cac1cb0350d3e30fe758
Kittithee Thaweesuk <kittithee2540@hotmail.com>
2020-03-15 04:02:34 +0700

    test deploy

--- resources/todo.py
+++ resources/todo.py
@@ -1,15 +1,9 @@
 from flask import jsonify, make_response
 from flask_restful import Resource
-import matplotlib.pyplot as plt
-import cv2
-import glob
-import numpy as np
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+
 
 
 class CNN(Resource):
     def get(self, id):
-                prediction = {"prediction": "Test"}
+        prediction = {"prediction": "Test"}
         return make_response(jsonify(prediction), 200)

379d4fe3543ef309c9a038561ebd5e29bbac691f
Kittithee Thaweesuk <kittithee2540@hotmail.com>
2020-03-19 17:00:50 +0700

    Re-test crop image

--- resources/todo.py
+++ resources/todo.py
@@ -14,25 +14,25 @@ class CNN(Resource):
         npimg = np.fromstring(img, dtype=np.uint8)
         source = cv2.imdecode(npimg, 1)
 
-        face_cascade = cv2.CascadeClassifier('https://github.com/kinnnzl/cnn-api/blob/master/resources'
-                                             '/haarcascade_frontalface_alt.xml')
-
-        grey_img = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
-
-        plt.figure(figsize=(20, 10))
-        plt.imshow(grey_img, cmap='gray')
-
-        face_detect = face_cascade.detectMultiScale(grey_img, scaleFactor=1.1, minNeighbors=5)
-        print('Face found : ', len(face_detect))
-
-        face_detect
-        n = 25
-        for (x, y, w, h) in face_detect:
-            cv2.rectangle(source, (x, y), (x + w + n, y + h + n), (0, 255, 0), 3)
-            crop_img = source[y:y + h + n, x:x + w + n]
-
-        plt.figure(figsize=(20, 10))
-        plt.imshow(cv2.cvtColor(source, cv2.COLOR_BGR2RGB))
+        # face_cascade = cv2.CascadeClassifier('https://github.com/kinnnzl/cnn-api/blob/master/resources'
+        #                                      '/haarcascade_frontalface_alt.xml')
+        #
+        # grey_img = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+        #
+        # plt.figure(figsize=(20, 10))
+        # plt.imshow(grey_img, cmap='gray')
+        #
+        # face_detect = face_cascade.detectMultiScale(grey_img, scaleFactor=1.1, minNeighbors=5)
+        # print('Face found : ', len(face_detect))
+        #
+        # face_detect
+        # n = 25
+        # for (x, y, w, h) in face_detect:
+        #     cv2.rectangle(source, (x, y), (x + w + n, y + h + n), (0, 255, 0), 3)
+        #     crop_img = source[y:y + h + n, x:x + w + n]
+        #
+        # plt.figure(figsize=(20, 10))
+        # plt.imshow(cv2.cvtColor(source, cv2.COLOR_BGR2RGB))
         # cv2.imwrite('2_501.jpg', crop_img)
 
         # ------------------------------------------ cnn ------------------------------------------------------------
